47008d49ea36192619bd7aa4000594933e579b2d
Sergey Korsakov <bpotmhehoru1@gmail.com>
2022-06-19 00:14:13 +0500

    Update 25.sh

--- 25.sh
+++ 25.sh
@@ -78,7 +78,7 @@ function eof_docker_compose {
         "--node-rpc-url", "ws://node:9944",
         "--ws-server-listen-addr", "0.0.0.0:9955",
         "--reward-address", "$WALLET_ADDRESS",
-        "--plot-size", "250"
+        "--plot-size", "250G"
       ]
   volumes:
     node-data:
